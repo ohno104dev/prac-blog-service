6d1b53ae318063261d1f6c01761ad345b1f14999
ohno <ohno104dev@gmail.com>
2021-02-09 19:17:49 +0800

    feat: blog-service develop using Gin
    1. construct router paths
    2. construct a standardized model for error outputs

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/internal/routers"
+)
+
+func main() {
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"message": "pong"})
+	// })
+	// r.Run()
+
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
