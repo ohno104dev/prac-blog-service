80bc5a58034a0c02f7a5e6629d6e8f1cb9c86d08
ohno <ohno104dev@gmail.com>
2021-02-10 00:39:35 +0800

    feat: construct config setting using Viper package
    1. add server config
    2. add app. config
    3. add database config

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "felix.bs.com/felix/BeStrongerInGO/02_GinBlog/pkg/setting"
+
+var (
+	ServerSetting    *setting.ServerSettings
+	AppSetting       *setting.AppSettings
+	DatabaseSettings *setting.DatabaseSettings
+)
--- main.go
+++ main.go
@@ -1,26 +1,67 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/global"
 	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/internal/routers"
+	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// r := gin.Default()
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{"message": "pong"})
-	// })
-	// r.Run()
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
 
+}
+
+func main() {
+	/*
+		router := routers.NewRouter()
+		s := &http.Server{
+			Addr:           ":8080",
+			Handler:        router,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+		s.ListenAndServe()
+	*/
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSettings)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
