284a7ff2c0429290fa64d9ca6e09b6d85fae6fc5
ohno <ohno104dev@gmail.com>
2021-02-11 11:48:52 +0800

    feat: construct log functions using Lumberjack package
    1. construct a standardized model for log outputs

--- global/setting.go
+++ global/setting.go
@@ -1,9 +1,14 @@
 package global
 
-import "felix.bs.com/felix/BeStrongerInGO/02_GinBlog/pkg/setting"
+import (
+	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/pkg/logger"
+	"felix.bs.com/felix/BeStrongerInGO/02_GinBlog/pkg/setting"
+)
 
 var (
 	ServerSetting    *setting.ServerSettings
 	AppSetting       *setting.AppSettings
 	DatabaseSettings *setting.DatabaseSettings
+
+	Logger *logger.Logger
 )
